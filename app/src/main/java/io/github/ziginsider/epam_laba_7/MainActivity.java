b3d49f322354c591af6510f9f27756df2ac2a9f6
ziginsider <ziginsider@gmail.com>
2018-04-02 13:20:07 +0300

    refactor MainActivity

--- app/src/main/java/io/github/ziginsider/epam_laba_7/MainActivity.java
+++ app/src/main/java/io/github/ziginsider/epam_laba_7/MainActivity.java
@@ -3,7 +3,6 @@ package io.github.ziginsider.epam_laba_7;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
